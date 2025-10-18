980301c85f880b67fe7c2794c80d867fa1a9edd7
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-10-19 04:07:48 +0800

    SystemUI: Blur rendering enhancements
    Change-Id: I024055b5ce6e0dc080e74b4d8b5598c05a2a56ba

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -1042,7 +1042,7 @@ public final class NotificationPanelViewController implements
                 mBlurRenderEffect = RenderEffect.createBlurEffect(
                         mBlurConfig.getMaxBlurRadiusPx(),
                         mBlurConfig.getMaxBlurRadiusPx(),
-                        Shader.TileMode.CLAMP);
+                        Shader.TileMode.MIRROR);
             }
             debugLog("Applying blur RenderEffect to shade.");
             Trace.asyncTraceForTrackBegin(Trace.TRACE_TAG_APP, "ShadeBlurRenderEffect", "active",
--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayoutController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayoutController.java
@@ -1477,7 +1477,7 @@ public class NotificationStackScrollLayoutController implements Dumpable {
             mView.setRenderEffect(RenderEffect.createBlurEffect(
                     blurRadius,
                     blurRadius,
-                    Shader.TileMode.CLAMP));
+                    Shader.TileMode.MIRROR));
         } else {
             debugLog("Resetting blur RenderEffect for NotificationStackScrollLayoutController");
             mView.setRenderEffect(null);
